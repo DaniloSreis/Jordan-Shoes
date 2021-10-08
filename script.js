a6dda20bdad0377711176c9eee637798834c200a
Danilo <81874991+DaniloSreis@users.noreply.github.com>
2021-10-08 18:02:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '50px',
+  duration: 2000,
+  reset: true
+})
+
+ScrollReveal().reveal('header', { delay: 200 })
+ScrollReveal().reveal('.destaques', { delay: 400 })
+ScrollReveal().reveal('.img-jordan', { delay: 500 })
